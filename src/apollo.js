ea296742d29b88696e3a7a8651de3f3fdc414e53
dream_JHL <jihyunlee.me@gmail.com>
2021-07-19 15:24:11 +0900

    [1.2] Home.js / getMovies Query

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/', // specifies the URL of our GraphQl server
+  uri: 'http://apollo-movie-server.herokuapp.com/', // specifies the URL of our GraphQl server
   cache: new InMemoryCache() //cache is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
 })
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,72 @@
-import React from 'react';
 import {useQuery, gql} from '@apollo/client';
+import styled from 'styled-components';
+import Movie from '../components/Movie'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 const GET_MOVIES = gql`
-  {
-  movies {
-    id
-    medium_cover_image
+{
+  Movies(limit: 10){
+   id
   }
 }
 `;
 
 const Home = () => {
-  const {loading} = useQuery(GET_MOVIES);
-  console.log(loading)
-    return 'Home'
+  const {loading, error, data} = useQuery(GET_MOVIES);
+  console.log(loading, error, data)
+  return (
+  <Container>
+    <Header>
+      <Title>Apollo 2021</Title>
+      <Subtitle>I love GraphQL</Subtitle>
+    </Header>
+    {loading && <Loading> Loading ... </Loading>} {/* if loading, show <loading></loading> */}
+    {!loading && data.Movies && data.Movies.map(
+      movie => {  
+        return (
+          <Movie 
+            key={movie.id}
+            id={movie.id} 
+          ></Movie>
+        )
+      }
+    )}
+  </Container>
+  );
 };
 
 export default Home;
