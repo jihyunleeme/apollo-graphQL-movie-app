a1970c7b08b716b1a90d0eddbac10e19521f731a
dream_JHL <jihyunlee.me@gmail.com>
2021-07-19 15:24:55 +0900

    [1.3] Detail.js / getMovie Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) { 
+    Movie(id : $id){
+    id
+    title
+  }
+  }
+`;
 
 const Detail = () => {
+  const { id } = useParams();
+  const {loading, data} = useQuery(GET_MOVIE, {
+    variables: { id }
+  });
+  console.log(loading, data)
+
+  if (loading) {return "loading";}
+  if (data && data.movie) {return data.movie.title;}
   return 'Detail'
 };
 
