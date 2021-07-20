0927b35937a76043af535431ede0b3fc0b36d58f
dream_JHL <jihyunlee.me@gmail.com>
2021-07-21 00:30:32 +0900

    [eslint] 린트 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
@@ -18,5 +19,5 @@ module.exports = {
         "react"
     ],
     "rules": {
-    }
+      }
 };
